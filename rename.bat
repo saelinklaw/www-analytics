0d00146c58efdbbd139dbe8fb074b2d9f8956f36
saelinklaw <72177966+saelinklaw@users.noreply.github.com>
2020-12-01 15:04:24 +0800

    Nov

---
+++ rename.bat
@@ -0,0 +1,13 @@
+﻿@echo off&setlocal enabledelayedexpansion
+echo,&set/p x=Step1. filename
+echo,&set/p sign=Step2. str
+echo,&set/p change=Step3. ch
+echo,&echo,process
+for %%i in (*.%x%) do (
+set "filename=%%i"
+if defined sign set "filename=!filename:%sign%=!"
+if defined change set "filename=!filename:%change:~,1%=%change:~-1%!"
+if not "%%i"=="!filename!" ren "%%i" "!filename!"
+)
+echo,ok
+pause>nul
